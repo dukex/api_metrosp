83df14e2d54500ed14f5c7421e813d14c5e86987
Duke <duke@riseup.net>
2012-06-06 02:17:30 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiMetro::Application.config.secret_token = '64046101d27e07e379afb0368ee1bf27379739ffaa976c9b547e871f98ec34e4c2b85784efc5e1c708d8a5515f91079b5a25a000b6debb44f764ffbfd60dd978'
