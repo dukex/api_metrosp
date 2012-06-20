0bd0458e974cd22e2f92202d1fafb218a82d2ee2
Duke <duke@riseup.net>
2012-06-20 02:29:49 -0300

    added representer default_url_options host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,9 @@ ApiMetro::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.representer.default_url_options = {:host => "127.0.0.1:3000"}
+
+
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
@@ -35,6 +38,4 @@ ApiMetro::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host =>
-"metro.com" }
 end
